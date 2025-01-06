e4eec92c9182fc2311996024992d16fdcb3e2a1a
8lak <123122715+8lak@users.noreply.github.com>
2025-01-06 15:27:17 -0500

    Update data_preprocessing.R

--- Unsupervised_Artist_Analysis/scripts/data_preprocessing.R
+++ Unsupervised_Artist_Analysis/scripts/data_preprocessing.R
@@ -2,25 +2,12 @@
 library(tidyverse)
 music <- read_csv("https://bcheggeseth.github.io/253_spring_2024/data/billboard.csv")
 
-music %>% 
-  count(performer) %>% 
-  filter(n >= 25) %>% 
-  select(performer)
-
-
-```
-
-```{r}
 my_artist <- music %>% 
   filter(performer == "Beyonce") %>% 
   select(-performer) %>% 
   group_by(song) %>%      
   slice_sample(n = 1) %>% 
   ungroup()
-
-```
-
-```{r}
 my_artist <- my_artist %>% 
   column_to_rownames("song")
 
